717cf22e1a30a6fccd0033f00604b721403d84aa
Erik Lundberg <erik.lumpa@hotmail.com>
2023-01-23 20:11:20 +0100

    fix header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,6 +8,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <Link href = '/users/login'> Login here! </Link>
+        <Link href = '/users/register'> Sign up here! </Link>
         <p> Hi </p>  
       </div>
     </main>
