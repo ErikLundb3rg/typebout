dfcb9d8207eecff627d4c36a9ac2b7043eb03eda
Erik Lundberg <56272948+ErikLundb3rg@users.noreply.github.com>
2023-05-04 22:39:45 +0200

    fix bug (#48)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,9 @@ import { AuthProvider } from '@/providers/useAuth'
 import { theme } from '@/theme'
 import Header from '@/components/header'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function RootLayout({
   children
 }: {
