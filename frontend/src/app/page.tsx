f04f7f55d3641f63d557ec42de47e6a888aa67d4
Erik Lundberg <erik.lumpa@hotmail.com>
2023-07-07 10:06:45 +0200

    change npm font usage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 import {
   Box,
   Stack,
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 import useAuth from '@/providers/useAuth'
 import {
   Box,
